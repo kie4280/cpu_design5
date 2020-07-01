4a4818c27d03da632bc2606d206d503c76c9e134
kie4280 <kie4280ann@gmail.com>
2020-07-01 22:52:35 +0800

    1. add new class Cache

--- src/direct_mapped_cache_lru.cpp
+++ src/direct_mapped_cache_lru.cpp
@@ -1,72 +1,77 @@
 // Author: 0710012 何權祐, 0710018 張宸愷
-#include <math.h>
-#include <stdio.h>
-
+#include <cmath>
+#include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 struct cache_content {
-  bool v;
+  bool v, dirty;
   unsigned int tag;
   unsigned int count = 0;
   // unsigned int	data[16];
 };
 
-const int K = 1024;
-
-double log2(double n) {
-  // log(n) / log(2) is log2.
-  return log(n) / log(double(2));
-}
-
-void simulate(int cache_size, int block_size, int n_way) {
-  unsigned int tag, index, x;
-
-  int offset_bit = (int)log2(block_size);
-  int set_bit = (int)log2(n_way);
-  int index_bit = (int)log2(cache_size / block_size);
-
-  int line = (cache_size >> offset_bit);  // rows
-  int accesses = 0, miss = 0;
-
-  cache_content **cache = new cache_content *[line >> set_bit];
-  for (int a = 0; a<line>> set_bit; ++a) {
-    cache[a] = new cache_content[n_way];
+class Cache {
+ private:
+  double log2(double n) {
+    // log(n) / log(2) is log2.
+    return log(n) / log(double(2));
   }
 
-  // cout << "cache line: " << line << endl;
+ public:
+  const int K = 1024;
+  int offset_bit;
+  int set_bit;
+  int index_bit;
+  int line;
+  int n_way;
+  cache_content **cache;
 
-  for (int j = 0; j < (line >> set_bit); j++) {
-    for (int i = 0; i < n_way; ++i) {
-      cache[j][i].v = false;
+  Cache(int cache_size, int block_size, int n_way) {
+    offset_bit = (int)log2(block_size);
+    set_bit = (int)log2(n_way);
+    index_bit = (int)log2(cache_size / block_size);
+    line = (cache_size >> offset_bit);  // rows
+    Cache::n_way = n_way;
+
+    cache = new cache_content *[line >> set_bit];
+    for (int a = 0; a<line>> set_bit; ++a) {
+      cache[a] = new cache_content[n_way];
+    }
+
+    for (int j = 0; j < (line >> set_bit); j++) {
+      for (int i = 0; i < n_way; ++i) {
+        cache[j][i].v = false;
+        cache[j][i].dirty = false;
+      }
     }
   }
 
-  FILE *fp = fopen("test/LU.txt", "r");  // read file
-
-  while (fp != 0 && fscanf(fp, "%x", &x) != EOF) {
+  bool access(unsigned int x) {
     // cout << hex << x << " ";
-    index = (x >> (offset_bit)) & ((line - 1)>>set_bit);
+    unsigned int tag, index;
+    index = (x >> (offset_bit)) & ((line - 1) >> set_bit);
     tag = x >> (index_bit + offset_bit - set_bit);
-    
+
     // unsigned int set_index = index >> set_bit;
-    
+
     unsigned int least_used = 0;
     int least_index = 0;
+    bool hit = false;
 
     for (int b = 0; b <= n_way; ++b) {
-      if (b < n_way && cache[index][b].v &&
-          cache[index][b].tag == tag) {
+      if (b < n_way && cache[index][b].v && cache[index][b].tag == tag) {
         cache[index][b].v = true;  // hit
         cache[index][b].count = 0;
-        
+        hit = true;
         break;
       }
 
       else if (b == n_way) {
         for (int j = 0; j < n_way; ++j) {
-          if(cache[index][j].v == false) {
+          if (cache[index][j].v == false) {
             least_index = j;
             break;
           }
@@ -78,38 +83,49 @@ void simulate(int cache_size, int block_size, int n_way) {
         cache[index][least_index].count = 0;
         cache[index][least_index].v = true;
         cache[index][least_index].tag = tag;
-        miss++;
       }
-
-      
     }
 
-    for(int b=0; b<n_way; ++b) {
+    for (int b = 0; b < n_way; ++b) {
       cache[index][b].count++;
     }
     // cout <<dec<< miss << " " << accesses << endl;
-    accesses++;
-  }
-  if (fp != 0) {
-    fclose(fp);
   }
 
-  // cout <<dec<< miss << " " << accesses << endl;
-  cout << (double)miss / accesses;
-  for (int a = 0; a<line>> set_bit; ++a) {
-    delete[] cache[a];
+  ~Cache() {
+    for (int a = 0; a<line>> set_bit; ++a) {
+      delete[] cache[a];
+    }
+    delete[] cache;
   }
-  delete[] cache;
+};
+
+Cache sys1_1(512, 32, 8);
+Cache sys2_1(512, 32, 8);
+Cache sys3_1(128, 16, 8), sys3_2(4096, 128, 8);
+
+
+int access_system1(unsigned int address) {
+  bool read1 = sys1_1.read(address);
+  if(read1)
+
 }
 
 int main(int argc, char **argv) {
-  // Let us simulate 4KB cache with 16B blocks
-  for (int a = 0; a <= 6; ++a) {
-    for (int b = 0; b < 4; ++b) {
-      simulate((4 << b * 2) * K, 64, 1 << a);
-      cout << " ";
-      // cout << (4<<b*2) << "           " <<endl;
-    }
-    cout << endl;
+  
+
+  unsigned int baseA, baseB, baseC;
+  int m, n, p;
+  fstream fs;
+  if (argc <= 2) {
+    return -1;
   }
+
+  fs.open(string(argv[1]), ios::in);
+
+
+
+  fs.close();
+
+  return 0;
 }
